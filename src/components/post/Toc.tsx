8f694a55c1c939e0b297ae257945366c089c6fbe
dxu <dean.xu@asml.com>
2021-08-24 10:44:16 +0800

    fix toc break word

--- src/components/post/Toc.tsx
+++ src/components/post/Toc.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {ProgressToc} from "common/components/ProgressToc";
 import {TocItem} from "src/domain";
+import clsx from "clsx";
 
 type Props = {
   toc: TocItem[]
@@ -9,25 +10,23 @@ type Props = {
 export const Toc = (props: Props) => {
   const {toc, className} = props
   return (
-    <div className={className}>
-      <div className={'break-words flex flex-col items-start max-w-[16rem]'}>
-        <a href={'#'} onClick={() => window.scrollTo(0, 0)}
-           className={'text-2xl mb-4 ml-6 hover:underline'}
-        >
-          目录
-        </a>
-        <ProgressToc>
-          <ul className={'ml-0'}>
-            {toc.map((e, i) => (
-              <li key={i} style={{marginLeft: (e.level - 2) * 15 + 5}}>
-                <a href={`#${e.anchor}`}>
-                  {e.text}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </ProgressToc>
-      </div>
+    <div className={clsx('break-all flex flex-col items-start max-w-[16rem]', className)}>
+      <a href={'#'} onClick={() => window.scrollTo(0, 0)}
+         className={'text-2xl mb-4 ml-6 hover:underline'}
+      >
+        目录
+      </a>
+      <ProgressToc>
+        <ul className={'ml-0'}>
+          {toc.map((e, i) => (
+            <li key={i} style={{marginLeft: (e.level - 2) * 15 + 5}}>
+              <a href={`#${e.anchor}`}>
+                {e.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </ProgressToc>
     </div>
   )
 }
