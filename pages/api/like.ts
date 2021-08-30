acf445ab9dd26080295fa6cfd411a91a7384b7fe
dxu <dean.xu@asml.com>
2021-08-30 10:14:39 +0800

    use common api

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -1,7 +1,6 @@
 import {apiHandler} from "common/api/handler";
-import {database} from "common/api/database";
-import {OkPacket} from "mysql";
 import {stringToBoolean} from "../../common/util/base";
+import {getLike, setLike} from "../../common/api/impl/like";
 
 
 export default apiHandler({
@@ -9,32 +8,20 @@ export default apiHandler({
     GET: async ({helper}) => {
       const postId = helper.museQuery('postId')
       const userId = helper.getUserId()
-      const sql = `SELECT COUNT(*) AS total, IFNULL(SUM(user_id = ?), 0) AS you 
-      FROM xdean.blog_post_like 
-      WHERE post_id = ?`;
-      const result = await database.query<{ total: number, you: number }[]>(sql, [userId, postId])
-      if (result.length === 0) {
-        return {
-          total: 0,
-          you: 0,
-        }
-      }
-      return result[0]
+      return await getLike({
+        objId: `blog/${postId}`,
+        userId
+      })
     },
     POST: async ({helper}) => {
       const postId = helper.museQuery('postId')
       const userId = helper.getUserId()
       const like = stringToBoolean(helper.museQuery('value', 'true'))
-      if (like) {
-        const sql = `INSERT IGNORE INTO xdean.blog_post_like(post_id, user_id)
-                     VALUES (?, ?)`
-        await database.query<OkPacket>(sql, [postId, userId])
-        return null
-      } else {
-        const sql = `DELETE FROM xdean.blog_post_like WHERE post_id = ? AND user_id=?`
-        await database.query<OkPacket>(sql, [postId, userId])
-        return null
-      }
+      await setLike({
+        objId: `blog/${postId}`,
+        userId,
+        like,
+      })
     }
   }
 })
--- pages/api/read.ts
+++ pages/api/read.ts
@@ -1,6 +1,5 @@
 import {apiHandler} from "common/api/handler";
-import {database} from "common/api/database";
-import {OkPacket} from "mysql";
+import {getRead} from "../../common/api/impl/read";
 
 
 export default apiHandler({
@@ -8,16 +7,10 @@ export default apiHandler({
     GET: async ({helper}) => {
       const postId = helper.museQuery('postId')
       const userId = helper.getUserId()
-
-      const insertSql = `INSERT IGNORE INTO xdean.blog_post_read(post_id, user_id)
-                         VALUES (?, ?)`
-      await database.query<OkPacket>(insertSql, [postId, userId])
-
-      const selectSql = `SELECT COUNT(*) AS total
-      FROM xdean.blog_post_read 
-      WHERE post_id = ?`;
-      const result = await database.query<{ read: number }[]>(selectSql, [postId])
-      return result[0]
+      return getRead({
+        objId: `blog/${postId}`,
+        userId
+      })
     },
   }
 })
