e905b9b3f891f6a80ce6cab1d322181ac01b85e9
Dean Xu <373216024@qq.com>
2022-03-27 20:39:39 +0800

    add baidu analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import {AppProps} from "next/dist/pages/_app";
-import '../styles/global.css'
-import Head from 'next/head'
-import {Layout} from "../src/components/layout/Layout";
-import {useRouter} from "next/router";
-import {useGA} from "../common/util/ga";
-import {CONSTANT} from "../src/constants";
-import {DefaultSeo} from "next-seo";
-
+import {AppProps} from 'next/dist/pages/_app';
+import '../styles/global.css';
+import Head from 'next/head';
+import {Layout} from '../src/components/layout/Layout';
+import {useRouter} from 'next/router';
+import {useGA} from '../common/util/ga';
+import {CONSTANT} from '../src/constants';
+import {DefaultSeo} from 'next-seo';
+import {isDEV} from '../common/util/env';
 
 function MyApp({Component, pageProps}: AppProps) {
-  const router = useRouter()
-  useGA(CONSTANT.gaId)
+  const router = useRouter();
+  useGA(CONSTANT.gaId);
   return (
     <>
       <DefaultSeo
@@ -19,20 +19,30 @@ function MyApp({Component, pageProps}: AppProps) {
       />
       <Head>
         <title>XDean NextJS Template</title>
+        {!isDEV && <script dangerouslySetInnerHTML={{
+          __html: `var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?18ff7e79ef1c492ff64063605453b2f4";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+`,
+        }}/>}
       </Head>
       {function () {
         if (router && router.query.layout === 'false') {
-          return <Component {...pageProps}/>
+          return <Component {...pageProps}/>;
         } else {
           return (
             <Layout>
               <Component {...pageProps}/>
             </Layout>
-          )
+          );
         }
       }()}
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
